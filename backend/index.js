b3bd423518106fc82780089c4902da56b19ca305
unknown <rapadantindustries@gmail.com>
2024-05-23 17:43:23 -0400

    missed file saves

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,12 @@ require( "dotenv" ).config()
 const express = require( "express"  )
 const session = require( "express-session" )
 const cors = require( "cors" )
+const jwt = require( "express-jwt" )
+const jwksRsa = require( "jwks-rsa" )
 const mongoose = require( "mongoose" )
 const { v4: uuidv4 } = require( "uuid" )
 
+//Express instance
 const app = express()
     app.use( cors() )
 
@@ -21,6 +24,23 @@ if ( app.get( 'env' ) === 'production' ) {
     app.set( 'trust proxy', 1 ) // trust first proxy
     sess.cookie.secure = true // serve secure cookies
 }
+
+const authConfig = {
+    domain: process.env.Auth0Domain,
+    audience: process.env.Auth0ClientId
+}
+
+const checkJwt = jwt( {
+    secret: jwksRsa.expressJwtSecret( {
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${ process.env.Auth0Domain }/.well-known/jwks.json`
+    } ),
+    audience: process.env.Auth0ClientId,
+    issuer: `https://${ process.env.Auth0Domain }/`,
+    algorithms: [ 'RS256' ]
+} )
 
 app.use( session( sess ) )
 
@@ -45,14 +65,15 @@ app.get( "/verify", ( req, res ) => {
     res.send( "verify in progress" )
 } )
 
+
 //Private Routes
-app.get( "/my-games", Authenticated, async ( req, res ) => {
+app.get( "/my-games", checkJwt, async ( req, res ) => {
     User.findOne( { userid: req.session.user.userId } ).then( user => {
         res.json( user.myGames || [ "default" ] )
     } )
 } )
 
-app.get( "/user-info", Authenticated, ( req, res ) => {
+app.get( "/user-info", checkJwt, ( req, res ) => {
     User.findOne( { userid: req.session.user.userId } ).then( user => {
         res.json( user || { default: true } )
     } )
