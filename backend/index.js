d6e17c8e3e84713856fd1af88c4dade424f83540
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2024-05-30 14:34:23 -0500

    Implemented login, registration and logout routes for backend, implemented JTW and user data response upon login. Implemented server side sessions for login. MongoDB implemented. More Verbose Backend console logging.

--- backend/index.js
+++ backend/index.js
@@ -2,53 +2,39 @@ require( "dotenv" ).config()
 
 const express = require( "express"  )
 const session = require( "express-session" )
+const bodyParser = require( "body-parser" )
+const bcrypt = require( "bcryptjs" )
+const jwt = require( "jsonwebtoken" )
 const cors = require( "cors" )
-const { expressjwt: jwt } = require( "express-jwt" )
-const jwksRsa = require( "jwks-rsa" )
 const mongoose = require( "mongoose" )
 const { v4: uuidv4 } = require( "uuid" )
 
 //Express instance
 const app = express()
-    app.use( cors() )
-
-const sess = {
-    secret: "Chicken Tenders",
-    genid: ( req ) => uuidv4(),
-    resave: true,
-    saveUninitialized: true,
-    cookie: {}
-}
-
-if ( app.get( 'env' ) === 'production' ) {
-    app.set( 'trust proxy', 1 ) // trust first proxy
-    sess.cookie.secure = true // serve secure cookies
-}
-
-const authConfig = {
-    domain: process.env.Auth0Domain,
-    audience: process.env.Auth0ClientId
-}
-
-const checkJwt = jwt( {
-    secret: jwksRsa.expressJwtSecret( {
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://${ process.env.Auth0Domain }/.well-known/jwks.json`
-    } ),
-    audience: process.env.Auth0ClientId,
-    issuer: `https://${ process.env.Auth0Domain }/`,
-    algorithms: [ 'RS256' ]
-} )
-
-app.use( session( sess ) )
+	app.use( bodyParser.json() )
+    app.use( cors( {
+		credentials: true
+	} ) )
+	if ( app.get( 'env' ) === 'production' )
+		app.set( 'trust proxy', 1 )
+	app.use( session( {
+		secret: "Chicken Tenders",
+		genid: ( req ) => uuidv4(),
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			secure: ( app.get( 'env' ) === 'production' ) || false
+		}
+	} ) )
 
 
 //Authentication Checker
 function Authenticated( req, res, next ) {
-    if ( req.session.user ) next()
-    else res.send( '0' )
+    if ( req.session && req.session.user ) {
+		next()
+	} else {
+		res.status( 401 ).send( { msg: "Not authenticated" } )
+	}
 }
 
 
@@ -58,35 +44,73 @@ const { User } = require( "./models/user.js" )
 
 //Public Routes
 app.post( "/login", ( req, res ) => {
-    res.send( { msg: "login in progress" } )
+	const { email, password } = req.body;
+
+	User.findOne( { email: email } ).then( user => {
+		if ( !user ) {
+			return res.status( 404 ).send( { msg: "User not found" } )
+		}
+
+		const passwordIsValid = bcrypt.compareSync( password, user.password )
+		if ( !passwordIsValid ) {
+			return res.status( 401 ).send( { msg: "Password is invalide" } )
+		}
+
+		const token = jwt.sign( { id: user.email }, "Chicken Tenders", { expiresIn: 86400} )
+		req.session.user = {
+			email: user.email,
+			name: user.name,
+			myGames: user.myGames
+		}
+		res.status( 200 ).send( { msg: "Login successful", user: req.session.user, token: token } )
+	} )
 } )
 
 app.post( "/register", ( req, res ) => {
-    res.send( { msg: "register in progress" } )
+	console.log( req.body )
+	const { email, password, confirmedPassword, name } = req.body;
+
+	if ( password === confirmedPassword ) {
+		return res.status( 401 ).send( { msg: "Password confirmation does not match" } )
+	}
+	
+	const hashedPassword = bcrypt.hashSync( password, 8 )
+	const newUser = new User( { name: name, email: email, password: hashedPassword } )
+	newUser.save()
+
+	res.status( 201 ).send( { msg: "User successfully registered" } )
 } )
 
 app.post( "/logout", ( req, res ) => {
-    res.send( { msg: "logout in progress" } )
+	req.session.destroy( err => {
+		if ( err ) {
+			return res.status( 500 ).send( { msg: "Logout failed" } )
+		}
+		res.status( 200 ).send( { msg: "Logout successful" } )
+	} )
 } )
 
 
 //Private Routes
-app.get( "/my-games", checkJwt, async ( req, res ) => {
+app.get( "/my-games", Authenticated, async ( req, res ) => {
     User.findOne( { userid: req.session.user.userId } ).then( user => {
         res.json( user.myGames || [ "default" ] )
     } )
 } )
 
-app.get( "/user-info", checkJwt, ( req, res ) => {
+app.get( "/user-info", Authenticated, ( req, res ) => {
     User.findOne( { userid: req.session.user.userId } ).then( user => {
         res.json( user || { default: true } )
     } )
 } )
 
-
+console.log( "Attempting to connect to MongoDB" )
 mongoose.connect( process.env.MongooseURL || 'mongodb://127.0.0.1:27017' ).then( () => {
-    app.listen( process.env.RestPort || 8080, () => {
+	console.log( "Successfully connected to MongoDB" )
+
+	app.listen( process.env.RestPort || 8080, () => {
         console.log( `Backend running on port: ${ process.env.RestPort || 8080 }` )
+		console.log( `Running in ${ app.get( 'env' ) } mode` )
     } )
 } )
 
