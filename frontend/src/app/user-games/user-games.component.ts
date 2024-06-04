2de3688f496da18baf5838a234a8aaa3f5b1d68c
MPBezerra <m.bezerra1015525@gmail.com>
2024-06-04 14:30:25 -0400

    Layout for game info page

--- frontend/src/app/user-games/user-games.component.ts
+++ frontend/src/app/user-games/user-games.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { BackendService } from '../services/backend.service';
 
 @Component({
   selector: 'app-user-games',
@@ -9,14 +11,40 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     MatPaginatorModule,
-    MatCardModule
+    MatCardModule,
+    RouterLink,
   ],
   templateUrl: './user-games.component.html',
   styleUrl: './user-games.component.scss'
 })
 export class UserGamesComponent {
 
-  games: any[] = new Array(100);
-  pageSize: number = 10;
+  length = 50;
+  pageSize = 25;
+  pageIndex = 0;
+  pageSizeOptions = [10, 25, 50, 100];
+  pageEvent?: PageEvent;
+  games: any[] = [];
+
+  constructor( private userService: BackendService ) { }
+
+  fetchGamesList(pageSize: number, pageIndex: number) {
+    this.userService.fetchMyGames().subscribe((games) => {
+      this.games = games.results;
+      this.length = games.number_of_total_results;
+      console.log(games.results);
+    })
+  }
+
+  ngOnInit() {
+    this.fetchGamesList(this.pageSize, this.pageIndex);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    this.fetchGamesList(this.pageSize, this.pageIndex);
+  }
 
 }
