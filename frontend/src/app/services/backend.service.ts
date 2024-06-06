1b5e9d5af68974adc47c4d50ae76cb1b46ae9e78
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2024-06-06 11:18:17 -0500

    rework backend service to nolonger use HttpParams. This poses a security risk as data was being sent through the body AND url. Now it sends only through the body

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 
@@ -18,89 +18,62 @@ export interface RegistationData {
 }
 
 @Injectable( {
-  providedIn: 'root'
+	providedIn: 'root'
 } )
 export class BackendService {
-  private backendURL: string = "http://localhost:8080"
+	private backendURL: string = "http://localhost:8080"
 
-  private baseHeaders = new HttpHeaders();
+	private baseHeaders = new HttpHeaders()
 
-  constructor( private http: HttpClient ) {
-	this.baseHeaders = this.baseHeaders.set('Access-Control-Allow-Origin', '*');
-	this.baseHeaders = this.baseHeaders.set('Content-Type', 'application/json');
-  }
+	constructor( private http: HttpClient ) {
+		this.baseHeaders = this.baseHeaders.set( 'Access-Control-Allow-Origin', '*' )
+		this.baseHeaders = this.baseHeaders.set( 'Content-Type', 'application/json' )
+	}
 
-  //This is bad, don't do this. Angular forced my hand with it's stupid HttpClient not wanting to work with post,
-  //Why on earth can't we just use fetch without problems? It's litterally built into every browser on eart.
-  //TODO: Rework to send passwords through normal http.post instead of through get as query parameters
-  login( data: LoginData ): Observable<any> {
-	let baseParams = new HttpParams();
+	login( data: LoginData ): Observable<any> {
+		return this.http.post<any>( `${ this.backendURL }/login`, { password: data.password, email: data.email }, {
+			headers: this.baseHeaders
+		} )
+	}
 
-	baseParams = baseParams.set( "email", data.email )
-	baseParams = baseParams.set( "password", data.password )
+	logout( data: LogoutData ): Observable<any> {
+		return this.http.post<any>( `${ this.backendURL }/logout`, { email: data.email }, {
+			headers: this.baseHeaders
+		} )
+	}
 
-	return this.http.post( `${ this.backendURL }/login`, data, {
-		headers: this.baseHeaders,
-		params: baseParams
-	} )
-  }
+	register( data: RegistationData ): Observable<any> {
+		return this.http.post( `${ this.backendURL }/register`, {
+			name: data.name,
+			email: data.email,
+			password: data.password,
+			passwordConfirmation: data.passwordConfirmation
+		}, {
+			headers: this.baseHeaders
+		} )
+	}
 
-  logout( data: LogoutData ): Observable<any> {
-	let baseParams = new HttpParams();
+	//Return an array of user's games that they favorited
+	fetchMyGames(): Observable<any> {
+		return this.http.get<any>( `${ this.backendURL }/my-game` )
+	}
 
-	baseParams = baseParams.set( "email", data.email )
+	addToMyGames( guid: string ): Observable<any> {
+		return this.http.get<any>( `${ this.backendURL }/my-game/add/${ guid }`, {
+			headers: this.baseHeaders
+		} )
+	}
 
-	return this.http.post( `${ this.backendURL }/logout`, data, {
-		headers: this.baseHeaders,
-		params: baseParams
-	} )
-  }
+	removeFromMyGames( guid: string ): Observable<any> {
+		return this.http.get<any>( `${this.backendURL}/my-game/remove/${ guid }`, {
+			headers: this.baseHeaders
+		} )
+	}
 
-  register( data: RegistationData ): Observable<any> {
-	let baseParams = new HttpParams();
-
-	baseParams = baseParams.set( "name", data.name )
-	baseParams = baseParams.set( "email", data.email )
-	baseParams = baseParams.set( "password", data.password )
-	baseParams = baseParams.set( "passwordConfirmation", data.passwordConfirmation )
-
-	return this.http.post( `${ this.backendURL }/register`, data, {
-		headers: this.baseHeaders,
-		params: baseParams
-	} )
-  }
-
-  //Return an array of user's games that they favorited
-  fetchMyGames(): Observable<any> {
-    return this.http.get<any>( `${ this.backendURL }/my-game` )
-  }
-
-  addToMyGames( guid: string ): Observable<any> {
-	let baseParams = new HttpParams();
-
-	baseParams = baseParams.set( "guid", guid )
-
-	return this.http.post<any>( `${ this.backendURL }/my-game/add`, guid, {
-		headers: this.baseHeaders,
-		params: baseParams
-	} )
-  }
-
-  removeFromMyGames( guid: string ): Observable<any> {
-	let baseParams = new HttpParams();
-
-	baseParams = baseParams.set( "guid", guid )
-
-	return this.http.post<any>( `${ this.backendURL }/my-game/remove`, guid, {
-		headers: this.baseHeaders,
-		params: baseParams
-	} )
-  }
-
-  //Return info about the currently logged in user
-  fetchUserInfo(): Observable<any> {
-    return this.http.get<any>( `${ this.backendURL }/user-info` )
-  }
+	//Return info about the currently logged in user
+	fetchUserInfo(): Observable<any> {
+		return this.http.get<any>( `${this.backendURL}/user-info` )
+	}
 }
 
 
