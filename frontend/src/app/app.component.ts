1e95f49a9596ac072cdb0501fbaf2aee34c69dee
MPBezerra <m.bezerra1015525@gmail.com>
2024-06-06 15:26:45 -0400

    Further styling of game info page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     RouterOutlet,
     NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
